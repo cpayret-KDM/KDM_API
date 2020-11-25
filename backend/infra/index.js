d230140bda3ec9cbf36708fe7afa46ec3bf765e3
Jimmy Jacobson <codingscape@gmail.com>
2020-11-25 13:33:36 -0800

    pulumi refactoring checkpoint

--- backend/infra/index.js
+++ backend/infra/index.js
@@ -1,59 +1,120 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi")
-const aws = require("@pulumi/aws")
 const awsx = require("@pulumi/awsx")
-const cloud = require("@pulumi/cloud")
+const aws = require("@pulumi/aws")
 
-//const vpc = awsx.ec2.Vpc.getDefault();
+/*******************************************************************************
+*   Configuration and Secrets
+*******************************************************************************/
 
-// Create a basic cluster and autoscaling group
-const vpc = new awsx.ec2.Vpc("kdm_vpc",{});
-const cluster = new awsx.ecs.Cluster("kdm_cloud", { vpc });
-//const cluster = new awsx.ecs.Cluster("custom", { vpc });
+let config = new pulumi.Config();
+const db = {
+    username: config.require("dbusername"),
+    password: config.requireSecret("dbpassword"),
+    db: config.require("dbdb")
+}
 
-const securityGroupIds = cluster.securityGroups.map(g => g.id);
+const springProfile = config.require("springProfile")
+const isFlyway = config.get("isFlyway") || "false"
+
+/*******************************************************************************
+*   Networking
+*******************************************************************************/
+const vpc = new awsx.ec2.Vpc("kdm_vpc",{})
+
+const cluster = new awsx.ecs.Cluster("kdm_cloud", { vpc })
+
+const securityGroupIds = cluster.securityGroups.map(g => g.id)
 
 const dbSubnets = new aws.rds.SubnetGroup("dbsubnets", {
+    vpc,
     subnetIds: vpc.publicSubnetIds,
+})
+
+
+
+
+const alb = new awsx.lb.ApplicationLoadBalancer("kdm-web-traffic", { vpc });
+
+const cert = new aws.acm.Certificate("kdm-cert", { 
+    validationMethod: "DNS",
+    domainName: alb.loadBalancer.dnsName.apply(domainName => `${domainName}`)
 });
 
-// Create a pgsql RDS instance and make it publicly accessible
+const target = alb.createTargetGroup("kdm-spring-target", { port: 8080, vpc })
+
+const listener = target.createListener("kdm-web-listener", { 
+    port: 443,
+    vpc,
+    certificateArn: cert.arn.apply(arn => `${arn}`)
+});
+
+/*******************************************************************************
+*   Create a pgsql RDS instance and make it publicly accessible
+*******************************************************************************/
 const kdmPgsqlInstance = new aws.rds.Instance("kdm-db", {
     allocatedStorage: 20,
     engine: "postgres",
     engineVersion: "12.4",
     instanceClass: "db.t2.micro",
-    name: "kdm",
-    //parameterGroupName: "kdm.postgresql12.4",
-    password: "cs_korth_pw",
+    name: db.db,
+    password: db.password,
     storageType: "gp2",
-    username: "cs_korth",
+    username: db.username,
     publiclyAccessible: true,
     skipFinalSnapshot: true,
     dbSubnetGroupName: dbSubnets.id,
     vpcSecurityGroupIds: securityGroupIds,
+    vpc
 })
 
-let service = new cloud.Service("kdm_api", {
+/*******************************************************************************
+*   ECR Repository and Docker Image Build/Push                                 * 
+*******************************************************************************/
+const repo = new awsx.ecr.Repository("kdm-repo")
+const image = repo.buildAndPushImage("../")
+
+/*******************************************************************************
+*   Deploy Containers to AWS ECS using Fargate
+*******************************************************************************/
+let service = new awsx.ecs.FargateService("kdm_api", {
     cluster: cluster,
-	containers: {
-		spring: {
-            build: "../",
-            args: {
-                "JAR_FILE": "./target/kdm_api-0.0.1-SNAPSHOT.jar",
-                "VERSION": "0.0.1"
-            },
-			memory: 1024,
-			ports: [{ port: 8080 }],
-			environment: {
-				"spring.datasource.url": `jdbc:postgresql://kdm-db18eda6d.cgaqu5r9jgep.us-west-2.rds.amazonaws.com:5432/kdm`,
-				"spring.datasource.username": "cs_korth",
-                "spring.datasource.password": "cs_korth_pw",
-                "spring.profiles.active": "no_kdm_security"
-			}
-		}
-	}
+    desiredCount: 1,
+    taskDefinitionArgs: {
+        containers: {
+            spring: {
+                image: image,
+                memory: 1024,
+                portMappings: [ listener ],
+                environment: [
+                    { 
+                        name: "spring.datasource.url",
+                        value: pulumi.all([kdmPgsqlInstance.endpoint]).apply(([server]) => `jdbc:postgresql://${server}/kdm`)
+                    },
+
+                    { 
+                        name: "spring.datasource.username",
+                        value: db.username
+                    },
+                    { 
+                        name: "spring.datasource.password",
+                        value: db.password
+                    },
+                    { 
+                        name: "spring.profiles.active",
+                        value: springProfile
+                    },
+                    {
+                        name: "spring.flyway.enabled",
+                        value: isFlyway
+                    }
+
+                ]
+            }
+        }
+    }
 })
 
 exports.db = kdmPgsqlInstance
-exports.url = service.defaultEndpoint.apply(e => `http://${e.hostname}`)
+exports.service = listener.endpoint
+exports.alb = alb
