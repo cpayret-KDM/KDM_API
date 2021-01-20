c2d25fad0732dff5f6b92d80fdbb21aadd38acf6
Jen Wilhelm <jen.wilhelm@gmail.com>
2021-01-20 13:50:00 -0800

    Properties: adds a new propertyType of Commercial.

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -31,6 +31,7 @@ export const PROPERTY_TYPE_MAP = {
   'SINGLE_FAMILY' : 'Single-Family',
   'MULTI_FAMILY' : 'Multi-Family',
   'OFFICE' : 'Office',
+  'COMMERCIAL' : 'Commercial',
   'INDUSTRIAL' : 'Industrial',
   'RETAIL' : 'Retail',
   'HOTEL' : 'Hotel',
