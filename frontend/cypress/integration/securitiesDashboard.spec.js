70812218efda05b53729e6b21ff7e8726941e5c5
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-01-26 17:14:41 -0800

    unsuccessfully clicking Securities Dashboard from drop-down for Cypress Tests

--- frontend/cypress/integration/securitiesDashboard.spec.js
+++ frontend/cypress/integration/securitiesDashboard.spec.js
@@ -7,8 +7,29 @@ describe('Security Dashboard', () => {
         // cy.visit('/securities/list')
     })
 
-    it('should have a securities list', () => {
-        // cy.visit('/securities/list')
-        cy.get('.security-list-table')
+
+    it('should have a securities drop-down button', () => {
+        cy.contains('Securities')
     })
+
+    it('should click the securities drop-down button', () => {
+        cy.contains('Securities').click()
+        // cy.contains('Dashboard').click()
+        // cy.contains('Dashboard').and('be.visible').click()
+    })
+
+    it('securities dashboard option should be visible', () => {
+        cy.contains('Securities').click()
+        cy.get('.side-nav-second-level').contains('Dashboard').and('be.visible').click()
+
+        // cy.get('.side-nav-second-level>span').find('>Dashboard')
+        // cy.contains('Dashboard').and('be.visible')
+        // cy.contains('.side-sub-nav-link').click()
+        // cy.get('Dashboard').click()
+    })
+
+    // it('should have a securities list', () => {
+    //     // cy.visit('/securities/list')
+    //     cy.get('.security-list-table')
+    // })
 })
