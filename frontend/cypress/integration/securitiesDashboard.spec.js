4e16ca2217092aa151a7441fb17eabea7b65dc32
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-01-26 17:38:26 -0800

    solved selecting Securities Dashboard button by using The Test Runner

--- frontend/cypress/integration/securitiesDashboard.spec.js
+++ frontend/cypress/integration/securitiesDashboard.spec.js
@@ -3,10 +3,7 @@ describe('Security Dashboard', () => {
     before(() => {
         cy.visit('/')
         cy.get('.btn').click()
-        // cy.get('span[data-text="Dashboard"]').click()
-        // cy.visit('/securities/list')
     })
-
 
     it('should have a securities drop-down button', () => {
         cy.contains('Securities')
@@ -14,22 +11,18 @@ describe('Security Dashboard', () => {
 
     it('should click the securities drop-down button', () => {
         cy.contains('Securities').click()
-        // cy.contains('Dashboard').click()
-        // cy.contains('Dashboard').and('be.visible').click()
     })
 
     it('securities dashboard option should be visible', () => {
         cy.contains('Securities').click()
-        cy.get('.side-nav-second-level').contains('Dashboard').and('be.visible').click()
-
-        // cy.get('.side-nav-second-level>span').find('>Dashboard')
-        // cy.contains('Dashboard').and('be.visible')
-        // cy.contains('.side-sub-nav-link').click()
-        // cy.get('Dashboard').click()
     })
 
-    // it('should have a securities list', () => {
-    //     // cy.visit('/securities/list')
-    //     cy.get('.security-list-table')
-    // })
+    it('securities dashboard option should be clickable', () => {
+        cy.get('.mm-active > .side-nav-second-level > :nth-child(1) > .side-nav-link-ref > span').click()
+    })
+
+    it('should have a securities list', () => {
+        // cy.visit('/securities/list')
+        cy.get('.security-list-table')
+    })
 })
