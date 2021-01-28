58657ae1b95fa48bde157fdd18abb32b9ac93c13
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-01-28 14:45:17 -0800

    Create Security Cypress tests complete

--- frontend/cypress/integration/securitiesCreate.spec.js
+++ frontend/cypress/integration/securitiesCreate.spec.js
@@ -1,6 +1,6 @@
 describe('Security Dashboard', () => {
 
-    before(() => {
+    beforeEach(() => {
         cy.visit('/')
         // cy.request('/account/login')
         cy.get('.btn').click()
@@ -11,28 +11,34 @@ describe('Security Dashboard', () => {
         cy.visit('/securities/create')
     })
 
-    it('should have all fields for new security creation', () => {
-        cy.get('#number')
-        cy.get('#noteRate')
-        cy.get(':nth-child(1) > .position-relative > .input-group > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')
-        cy.get(':nth-child(2) > .position-relative > .input-group > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')
-    })
-
     it('should have Create New Security buttons', () => {
+        cy.contains('Securities')
+        cy.visit('/securities/create')
         cy.get('.d-flex > .btn')
         cy.get('.breadcrumb > :nth-child(3) > a')
-    })
-
-    it('should have Save New Security button', () => {
-        cy.get(':nth-child(1) > .btn-primary')
     })
 
     it('Create New Security button should be clickable', () => {
         cy.get('.d-flex > .btn').click()
     })
 
+    it('should have all fields for new security creation', () => {
+        cy.contains('Securities')
+        cy.visit('/securities/create')
+        cy.get('#number')
+        cy.get('#noteRate')
+        cy.get(':nth-child(1) > .position-relative > .input-group > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')
+        cy.get(':nth-child(2) > .position-relative > .input-group > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')
+    })
+
     it('Save New Security button should be clickable', () => {
         cy.get(':nth-child(1) > .btn-primary').click()
     })
+
+    it('should have Save New Security button', () => {
+        cy.get(':nth-child(1) > .btn-primary')
+    })
+
+
 
 })
