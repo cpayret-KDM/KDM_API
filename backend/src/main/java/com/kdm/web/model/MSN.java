3c95c3f170b3ad94f08eee210bbb4ee648f48db0
Diego Bolanos <diego@codingscape.com>
2021-02-01 17:20:26 -0600

    Issue when merging

--- backend/src/main/java/com/kdm/web/model/MSN.java
+++ backend/src/main/java/com/kdm/web/model/MSN.java
@@ -14,6 +14,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
