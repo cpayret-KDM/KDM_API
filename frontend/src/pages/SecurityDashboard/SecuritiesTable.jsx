be99498aa8944fb090b135ccd06eafb9afea0d4b
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-02-02 12:33:00 -0800

    added CUSIP field to Securities Dashboard

--- frontend/src/pages/SecurityDashboard/SecuritiesTable.jsx
+++ frontend/src/pages/SecurityDashboard/SecuritiesTable.jsx
@@ -40,6 +40,16 @@ const SecuritiesTable = (props) => {
       footer: '',
     },
     {
+      dataField: 'cusipID',
+      text: 'CUSIP',
+      sort: true,
+      style: { width: '50px' },
+      filter: textFilter({
+        placeholder: ' ',
+      }),
+      footer: '',
+    },
+    {
       dataField: 'noteRate',
       text: 'Note Rate',
       sort: false,
