ea58a79cce1cd62ff9a7fa09e63c0f63ae681385
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-02-11 14:12:57 -0800

    added Loans and Securities specificity for respective Dashboard headers/labels

--- frontend/src/pages/LoanDashboard/LoanDashboard.jsx
+++ frontend/src/pages/LoanDashboard/LoanDashboard.jsx
@@ -61,7 +61,7 @@ const LoanDashboard = (props) => {
           breadCrumbItems={[
             {...breadcrumb},
           ]}
-          title="Dashboard"
+          title="Loans Dashboard"
         />
 
         <Row>
