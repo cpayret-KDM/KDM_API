e91fc739718369a8035d909f1cd15f90e3378421
Ryan Meinzer - Codingscape <ryan@codingscape.com>
2021-02-23 14:40:39 -0800

    integrated cypress test for negative value, nearly complete

--- frontend/cypress/integration/loanDashboard.spec.js
+++ frontend/cypress/integration/loanDashboard.spec.js
@@ -1,12 +1,18 @@
 describe('Loan Dashboard', () => {
 
-    before(() => {
+    beforeEach(() => {
         cy.visit('/')
         cy.get('.btn').click()
     })
 
     it('should have a loan list', () => {
         cy.get('.loan-list-table')
+    })
+
+    it('not have any negative spread values', () => {
+        cy.get('.loan-list-table')
+        // need to check for -1 through -9
+        cy.contains('-').should('not.exist')
     })
 
 })
